90c46d2e4506e63f907a71bd45f58508d18d6acc
Artemiy <artem-itf@yandex.ru>
2022-02-16 16:28:36 +0300

    Add java project skeleton

---
+++ src/main/java/ru/notlebedev/webprak/WebprakApplication.java
@@ -0,0 +1,13 @@
+package ru.notlebedev.webprak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebprakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/notlebedev/webprak/WebprakApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.notlebedev.webprak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

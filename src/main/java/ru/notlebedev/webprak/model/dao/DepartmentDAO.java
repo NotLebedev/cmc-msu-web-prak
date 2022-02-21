fe59c8a26d5571575201487271fe29b1deb37a64
Artemiy <artem-itf@yandex.ru>
2022-02-21 16:39:33 +0300

    Add DepartmentDAO interface

--- src/main/java/ru/notlebedev/webprak/model/dao/DepartmentDAO.java
+++ src/main/java/ru/notlebedev/webprak/model/dao/DepartmentDAO.java
@@ -1,7 +1,11 @@
 package ru.notlebedev.webprak.model.dao;
 
 import ru.notlebedev.webprak.model.entity.Department;
+import ru.notlebedev.webprak.model.entity.Department.Status;
+
+import java.util.Collection;
 
 public interface DepartmentDAO extends GenericDAO<Department, Long> {
-
+    Collection<Department> getDepartmentByName(String name);
+    Collection<Department> getDepartmentByNameByStatus(String name, Status status);
 }

abc1f9ada46c3f265fc92440ee4ad7409affbb67
Artemiy <artem-itf@yandex.ru>
2022-02-21 20:17:04 +0300

    Add PositionDAO interface and implementation

---
+++ src/main/java/ru/notlebedev/webprak/model/dao/PositionDAO.java
@@ -0,0 +1,6 @@
+package ru.notlebedev.webprak.model.dao;
+
+import ru.notlebedev.webprak.model.entity.Position;
+
+public interface PositionDAO extends GenericDAO<Position, Long> {
+}
---
+++ src/main/java/ru/notlebedev/webprak/model/dao/impl/PositionDAOImpl.java
@@ -0,0 +1,8 @@
+package ru.notlebedev.webprak.model.dao.impl;
+
+import ru.notlebedev.webprak.model.dao.PositionDAO;
+import ru.notlebedev.webprak.model.entity.Position;
+
+public class PositionDAOImpl extends GenericDAOImpl<Position, Long>
+    implements PositionDAO {
+}

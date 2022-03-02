56863f859a79d6f13781ec6a31e198470cb198e6
Artemiy <artem-itf@yandex.ru>
2022-03-02 17:48:18 +0300

    Configure health and index for thymeleaf

--- src/main/java/ru/notlebedev/webprak/web/StaticResourceConfiguration.java
+++ src/main/java/ru/notlebedev/webprak/web/StaticResourceConfiguration.java
@@ -1,8 +1,14 @@
 package ru.notlebedev.webprak.web;
 
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,7 +16,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class StaticResourceConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/health.html").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/**").addResourceLocations("file:./src/main/resources/static/");
         registry.addResourceHandler("/reports/**").addResourceLocations("file:./build/reports/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/notFound").setViewName("redirect:/");
+        registry.addViewController("/health").setViewName("health");
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
+        return container -> container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,
+                "/notFound"));
     }
 }

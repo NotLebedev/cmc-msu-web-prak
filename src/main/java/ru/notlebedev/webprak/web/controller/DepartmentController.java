e3b1a31480ed8b10e1c5973853333f3babc4bef7
Artemiy <artem-itf@yandex.ru>
2022-04-17 14:07:51 +0300

    Fix update and create in department

--- src/main/java/ru/notlebedev/webprak/web/controller/DepartmentController.java
+++ src/main/java/ru/notlebedev/webprak/web/controller/DepartmentController.java
@@ -4,20 +4,31 @@ import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.notlebedev.webprak.model.dao.DepartmentDAO;
+import ru.notlebedev.webprak.model.dao.PositionDAO;
+import ru.notlebedev.webprak.model.dao.PositionHistoryDAO;
 import ru.notlebedev.webprak.model.entity.Department;
+import ru.notlebedev.webprak.model.entity.Position;
+import ru.notlebedev.webprak.model.entity.PositionHistoryEntry;
 
 import java.util.Collection;
+import java.sql.Date;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Controller
 public class DepartmentController {
     private final DepartmentDAO departmentDAO;
+    private final PositionDAO positionDAO;
+    private final PositionHistoryDAO positionHistoryDAO;
 
-    public DepartmentController(DepartmentDAO departmentDAO) {
+    public DepartmentController(DepartmentDAO departmentDAO, PositionDAO positionDAO,
+                                PositionHistoryDAO positionHistoryDAO) {
         this.departmentDAO = departmentDAO;
+        this.positionDAO = positionDAO;
+        this.positionHistoryDAO = positionHistoryDAO;
     }
 
     @GetMapping(value = "/departments/department")
@@ -31,7 +42,7 @@ public class DepartmentController {
 
         if (id == null) {
             model.addAttribute("mode", Mode.CREATE);
-
+            model.addAttribute("department", new DepartmentEntry());
             return "department";
         }
 
@@ -47,6 +58,74 @@ public class DepartmentController {
         return "department";
     }
 
+    @PostMapping(value = "/departments/department")
+    public String department(
+            @RequestParam(value = "mode") String mode,
+            @RequestParam(value = "id", required = false) Long id,
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "superName", required = false) String superName,
+            Model model) {
+
+        Long newId = 0L;
+
+        if(mode.equals("UPDATE")) {
+            Optional<Department> dep = departmentDAO.findById(id);
+            if (dep.isEmpty())
+                return "error";
+
+            Department department = dep.get();
+            if (name != null)
+                department.setName(name);
+            if (superName != null) {
+                Collection<Department> superDep = departmentDAO.getByFilter(DepartmentDAO.Filter.builder()
+                        .name(superName)
+                        .build());
+                if (superDep.size() == 1)
+                    department.setDepartmentSuper(superDep.stream().findAny().get());
+            }
+
+            departmentDAO.updateSave(department);
+
+            newId = department.getId();
+        } else if (mode.equals("CREATE")) {
+            Department department = new Department(name, Department.Status.ACTIVE);
+
+            if (superName != null) {
+                Collection<Department> superDep = departmentDAO.getByFilter(DepartmentDAO.Filter.builder()
+                        .name(superName)
+                        .build());
+                if (superDep.size() == 1)
+                    department.setDepartmentSuper(superDep.stream().findAny().get());
+            }
+
+            departmentDAO.save(department);
+            newId = department.getId();
+        } else if (mode.equals("DELETE")) {
+            Optional<Department> dep = departmentDAO.findById(id);
+            if (dep.isEmpty())
+                return "error";
+
+            Department department = dep.get();
+
+            // Close all positions
+            department.getPositions().stream()
+                    .peek(position -> position.setStatus(Position.Status.CLOSED))
+                    .peek(positionDAO::save)
+                    .flatMap(position -> position.getPositionHistory().stream())
+                    .filter(e -> e.getStatus().equals(PositionHistoryEntry.Status.ACTIVE))
+                    .peek(e -> e.setStatus(PositionHistoryEntry.Status.FINISHED))
+                    .peek(e -> e.setDateEnd(new Date(System.currentTimeMillis())))
+                    .forEach(positionHistoryDAO::save);
+
+            department.setStatus(Department.Status.DEFUNCT);
+            departmentDAO.updateSave(department);
+
+            return "redirect:/departments";
+        }
+
+        return "redirect:/departments";
+    }
+
     public enum Mode {UPDATE, CREATE}
 
     @Getter
@@ -56,6 +135,14 @@ public class DepartmentController {
         private final String superName;
         private final Long superId;
         private final Department.Status status;
+
+        private DepartmentEntry() {
+            id = null;
+            name = null;
+            superName = null;
+            superId = null;
+            status = null;
+        }
 
         private DepartmentEntry(Department department) {
             id = department.getId();

3a6ad07d3dc3257f1e9f3ced8ea437618a216e74
Artemiy <artem-itf@yandex.ru>
2022-04-21 20:36:39 +0300

    Extract WebDriver bean

--- src/test/java/ru/notlebedev/webprak/web/BaseSeleniumTest.java
+++ src/test/java/ru/notlebedev/webprak/web/BaseSeleniumTest.java
@@ -1,12 +1,10 @@
 package ru.notlebedev.webprak.web;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.TestPropertySource;
@@ -18,24 +16,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BaseSeleniumTest {
     @LocalServerPort
     private int port;
+    @Autowired
     private WebDriver driver;
-    private String base;
-
-    @BeforeEach
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        System.setProperty("webdriver.firefox.driver", "geckodriver.exe");
-
-        FirefoxOptions options = new FirefoxOptions();
-        options.setHeadless(true);
-
-        driver = new FirefoxDriver(options);
-        this.base = "http://localhost:" + port;
-    }
 
     @Test
     public void testTest() {
-        driver.get(base);
+        driver.get("localhost:" + port + "/");
         WebElement element = driver.findElement(By.tagName("h1"));
         assertEquals("Добро пожаловать.", element.getText());
     }
---
+++ src/test/java/ru/notlebedev/webprak/web/WebDriverConfigurator.java
@@ -0,0 +1,21 @@
+package ru.notlebedev.webprak.web;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebDriverConfigurator {
+    @Bean
+    public WebDriver webDriver() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        System.setProperty("webdriver.firefox.driver", "geckodriver.exe");
+
+        FirefoxOptions options = new FirefoxOptions();
+        options.setHeadless(true);
+
+        return new FirefoxDriver(options);
+    }
+}

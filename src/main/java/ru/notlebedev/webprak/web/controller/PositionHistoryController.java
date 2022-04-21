cf60ae2ae5b1de27f7caf2fe8cb0010db7c333be
Artemiy <artem-itf@yandex.ru>
2022-04-21 13:22:03 +0300

    Implement assignment and firing logic

--- src/main/java/ru/notlebedev/webprak/web/controller/PositionHistoryController.java
+++ src/main/java/ru/notlebedev/webprak/web/controller/PositionHistoryController.java
@@ -1,9 +1,11 @@
 package ru.notlebedev.webprak.web.controller;
 
 import lombok.Getter;
+import lombok.NonNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.notlebedev.webprak.model.dao.EmployeeDAO;
 import ru.notlebedev.webprak.model.dao.PositionDAO;
@@ -14,6 +16,7 @@ import ru.notlebedev.webprak.model.entity.Position;
 import ru.notlebedev.webprak.model.entity.PositionHistoryEntry;
 import ru.notlebedev.webprak.util.FinalNonNullPair;
 
+import java.util.Comparator;
 import java.util.Date;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -41,6 +44,7 @@ public class PositionHistoryController {
 
         Employee employee = emp.get();
         model.addAttribute("empName", employee.getName());
+        model.addAttribute("empId", employee.getId());
         model.addAttribute("list", employee.getPositions().stream()
                 .peek(positionHistoryDAO::initialize)
                 .map(PositionListEntry::new)
@@ -58,16 +62,51 @@ public class PositionHistoryController {
                         e.getId()))
                 .collect(Collectors.toList()));
 
-
-
         return "history";
     }
 
+    @PostMapping(value = "/positions/history")
+    public String history(
+            @RequestParam(value = "id") Long employeeId,
+            @RequestParam(value = "mode") String mode,
+            @RequestParam(value = "newPosition", required = false) Long newPosition,
+            Model model) {
+        Optional<Employee> emp = employeeDAO.findById(employeeId);
+
+        if (emp.isEmpty())
+            return "error";
+
+        Employee employee = emp.get();
+        // Fire from existing in any case
+        if (mode.equals("FIRE") || mode.equals("ASSIGN")) {
+            employee.getPositions().stream()
+                    .filter(e -> e.getStatus().equals(PositionHistoryEntry.Status.ACTIVE))
+                    .peek(e -> e.setDateEnd(new java.sql.Date(System.currentTimeMillis())))
+                    .peek(e -> e.setStatus(PositionHistoryEntry.Status.FINISHED))
+                    .forEach(positionHistoryDAO::updateSave);
+        }
+
+        // In case of assignment assign to new
+        if (mode.equals("ASSIGN")) {
+            Optional<Position> pos = positionDAO.findById(newPosition);
+            if (pos.isEmpty())
+                return "error";
+
+            Position position = pos.get();
+            PositionHistoryEntry newEntry = new PositionHistoryEntry(position, employee,
+                    PositionHistoryEntry.Status.ACTIVE, new java.sql.Date(System.currentTimeMillis()));
+
+            positionHistoryDAO.save(newEntry);
+        }
+
+        return history(employeeId, model);
+    }
     @Getter
     private class PositionListEntry implements Comparable<PositionListEntry> {
         private final Long departmentId;
         private final String departmentName;
         private final String name;
+        private final Long id;
         private final String dateStart;
         private final String dateEnd;
         private final boolean isCurrent;
@@ -87,11 +126,13 @@ public class PositionHistoryController {
                 isCurrent = true;
             }
             date = entry.getDateStart();
+            id = entry.getId();
         }
 
         @Override
-        public int compareTo(PositionListEntry o) {
-            return date.compareTo(o.date);
+        public int compareTo(@NonNull PositionListEntry o) {
+            return Comparator.comparing(PositionListEntry::getDate).
+                    thenComparing(PositionListEntry::getId).compare(this, o);
         }
     }
 }

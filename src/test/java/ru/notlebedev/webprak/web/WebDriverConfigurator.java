d77c59c35941990d14ce8a67e9d61bcd06c84b66
artemiy <artem-itf@yandex.ru>
2022-04-30 20:48:52 +0300

    Add parameters for web driver in tests

--- src/test/java/ru/notlebedev/webprak/web/WebDriverConfigurator.java
+++ src/test/java/ru/notlebedev/webprak/web/WebDriverConfigurator.java
@@ -1,21 +1,41 @@
 package ru.notlebedev.webprak.web;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebDriverConfigurator {
+    @Value("${test_driver_firefox:geckodriver}")
+    private String geckodriver;
+    @Value("${test_driver_chrome:chromedriver}")
+    private String chromedriver;
+    @Value("${test_driver:geckodriver}")
+    private String driver;
+
     @Bean
     public WebDriver webDriver() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        System.setProperty("webdriver.firefox.driver", "geckodriver.exe");
+        System.setProperty("webdriver.chrome.driver", chromedriver);
+        System.setProperty("webdriver.firefox.driver", geckodriver);
 
-        FirefoxOptions options = new FirefoxOptions();
-        options.setHeadless(false);
-
-        return new FirefoxDriver(options);
+        switch (driver) {
+            case "geckodriver": {
+                FirefoxOptions options = new FirefoxOptions();
+                options.setHeadless(false);
+                return new FirefoxDriver(options);
+            }
+            case "chromedriver": {
+                ChromeOptions options = new ChromeOptions();
+                options.setHeadless(true);
+                return new ChromeDriver(options);
+            }
+            default:
+                throw new IllegalStateException("Unknown web driver type");
+        }
     }
 }
